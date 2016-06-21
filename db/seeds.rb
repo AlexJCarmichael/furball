adeeed27f20433ae96905021ccb62ab9e92e3bfc
Dane Carmichael <carmichaeldane@gmail.com>
2016-06-21 15:17:59 -0500

    Adding the refile, and refile-s3 gems

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,4 @@
 
-user = User.create!(name: "Geprge", address: "3 Reginald Lane", username: "test", cell_number: "(555) 555-5555",
+user = User.create!(name: "George", address: "3 Reginald Lane", username: "test", cell_number: "(555) 555-5555",
                     zip_code: "5555", email:  "test_user@test.test", password: "password")
 Pet.create!(user_id: user.id, name: "Sora", desc: "Fat, tortoise shell, kitty", category: "Cat", weight: "Damn")
