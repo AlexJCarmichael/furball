2971f0ae23528ea768e0e142c63a6094262dd5b8
Dane Carmichael <carmichaeldane@gmail.com>
2016-06-21 16:54:25 -0500

    Adding the required nonesense to make Refile and Refile-s3 work

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
+  attachment :profile_image
+
   has_secure_password
+  
   has_many :pets
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160621161715) do
+ActiveRecord::Schema.define(version: 20160621202017) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,16 +29,17 @@ ActiveRecord::Schema.define(version: 20160621161715) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "name",            null: false
-    t.string   "address",         null: false
+    t.string   "name",             null: false
+    t.string   "address",          null: false
     t.string   "username"
     t.string   "cell_number"
-    t.string   "zip_code",        null: false
-    t.string   "email",           null: false
-    t.string   "password_digest", null: false
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
+    t.string   "zip_code",         null: false
+    t.string   "email",            null: false
+    t.string   "password_digest",  null: false
+    t.datetime "created_at",       null: false
+    t.datetime "updated_at",       null: false
     t.string   "profile_image"
+    t.string   "profile_image_id"
   end
 
   add_foreign_key "pets", "users"
