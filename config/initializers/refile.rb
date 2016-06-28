37a0b702deaa47510a31c0a5241b8abb950582ed
Dane Carmichael <carmichaeldane@gmail.com>
2016-06-28 12:10:43 -0500

    Removing s3 references from project

--- config/initializers/refile.rb
+++ config/initializers/refile.rb
@@ -1,12 +1,12 @@
-require "refile/s3"
-
-
-aws = {
-  access_key_id: Rails.application.secrets.aws_access_key_id,
-  secret_access_key: Rails.application.secrets.aws_secret_access_key_id,
-  region: Rails.application.secrets.aws_region,
-  bucket: Rails.application.secrets.aws_bucket,
-}
-
-Refile.cache = Refile::S3.new(prefix: "cache", **aws)
-Refile.store = Refile::S3.new(prefix: "store", **aws)
+# require "refile/s3"
+#
+#
+# aws = {
+#   access_key_id: Rails.application.secrets.aws_access_key_id,
+#   secret_access_key: Rails.application.secrets.aws_secret_access_key_id,
+#   region: Rails.application.secrets.aws_region,
+#   bucket: Rails.application.secrets.aws_bucket,
+# }
+#
+# Refile.cache = Refile::S3.new(prefix: "cache", **aws)
+# Refile.store = Refile::S3.new(prefix: "store", **aws)
