7d0c33d33c21533a17d0db9756b78394c9009ad3
Dane Carmichael <carmichaeldane@gmail.com>
2016-07-06 16:07:31 -0500

    Adding a method to save the image to the user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,6 +9,7 @@ class UsersController < ApplicationController
 
   def create
     user = User.new(user_params)
+    user.profile_image = StringIO.new(params.fetch(:user).fetch(:profile_image))
     if user.save
       render json: user
     else
