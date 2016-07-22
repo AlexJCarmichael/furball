c903c1269dc60772806ee34da08d2796ca2a18fe
Dane Carmichael <carmichaeldane@gmail.com>
2016-07-22 10:50:40 -0500

    allowing any http method

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,16 @@
 Rails.application.routes.draw do
   resources :pets
-  resources :users
+  resources :users do
+    collection do
+      get :index
+      post :create
+    end
+    member do
+      get :show
+      get :edit
+      put :update
+      delete :destroy
+    end
+  end
   root "users#index"
 end
