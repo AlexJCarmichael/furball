1282209c7ecfcb6d8eebe9e0bd4bd7a7c15ebb2c
Dane Carmichael <carmichaeldane@gmail.com>
2016-09-08 11:28:16 -0500

    Adding the id of an object as part of the JSON response

--- app/serializers/pet_serializer.rb
+++ app/serializers/pet_serializer.rb
@@ -1,4 +1,11 @@
 class PetSerializer < ActiveModel::Serializer
-  attributes :name, :desc, :category, :breed, :weight, :lost_at
+  attributes :id,
+             :name,
+             :desc,
+             :category,
+             :breed,
+             :weight,
+             :lost_at
+             
   belongs_to :user
 end
--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,5 +1,6 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :email,
+  attributes :id,
+             :email,
              :name,
              :username,
              :cell_number,
